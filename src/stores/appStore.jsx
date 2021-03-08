36fccf995127a5e91e27e1b957e2ee025750451e
Juan Carlos Lamas Alfaro <juank.yea@hotmail.com>
2021-03-08 16:00:54 -0300

    added LU to base options

--- src/stores/appStore.jsx
+++ src/stores/appStore.jsx
@@ -7,8 +7,8 @@ import * as R from 'ramda'
  */
 const AppContext = createContext();
 
-const BASE_OPTIONS = ['Mera', 'Giovanni', 'JP', 'Juan K', 'Jesús', 'Edgar'];
-const BASE_ROBIN_OPTIONS = ['Paloma', 'Benjamín', 'Guido', 'Vicente', 'LU'];
+const BASE_OPTIONS = ['Mera', 'Giovanni', 'JP', 'Juan K', 'Jesús', 'Edgar', 'LU'];
+const BASE_ROBIN_OPTIONS = ['Paloma', 'Benjamín', 'Guido', 'Vicente'];
 
 /**
  * App Provider.
