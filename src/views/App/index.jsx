49b41263ee0c4d304a8114056798bca4328ef30f
cococov <juank.yea@hotmail.com>
2021-03-28 23:11:19 -0300

    firebase implemented

--- src/views/App/index.jsx
+++ src/views/App/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import firebase from '@firebase/app';
+import '@firebase/database';
 import { ThemeProvider } from 'styled-components';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { AppProvider } from '../../stores';
@@ -11,11 +13,22 @@ import {
   OptionsContainer
 } from './style';
 
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DB_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+});
+
 const App = () => {
   return (
-    <AppProvider>
-      <ThemeProvider theme={theme}>
-        <MuiThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
+      <MuiThemeProvider theme={theme}>
+        <AppProvider>
           <MainContainer>
             <RouletteContainer>
               <ControlBar />
@@ -27,9 +40,9 @@ const App = () => {
               <Options />
             </OptionsContainer>
           </MainContainer>
-        </MuiThemeProvider>
-      </ThemeProvider>
-    </AppProvider>
+        </AppProvider>
+      </MuiThemeProvider>
+    </ThemeProvider>
   );
 }
 
